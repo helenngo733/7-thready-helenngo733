b6b6efa57ee0ee08748f28f39a8b548b34d784b4
helenngo733 <helenngo@csu.fullerton.edu>
2024-10-23 17:50:32 -0700

    updated with allocated memory

--- thready.c
+++ thready.c
@@ -16,16 +16,17 @@ void* thread_func1(void* arg) {
     fprintf(stderr, "Thread %d (%ld) beginning\n", *ix, pthread_self());
     random_sleep();
     fprintf(stderr, "Thread %d (%ld) exiting\n", *ix, pthread_self());
+    free(arg);
     return NULL;
 }
 
 void pattern_1(int num_of_things) {
-    pthread_t threads[num_of_things]; //array
-    int thread_args[num_of_things]; //static array
+    pthread_t threads[num_of_things];  // array
 
     for (int ix = 0; ix < num_of_things; ix++) {
-        thread_args[ix] = ix + 1; //filling the array
-        pthread_create(&threads[ix], NULL, thread_func1, &thread_args[ix]);
+        int* thread_arg = malloc(sizeof(int));  // allocate memory
+        *thread_arg = ix + 1;                   // fill the memory
+        pthread_create(&threads[ix], NULL, thread_func1, thread_arg);
     }
 
     for (int ix = 0; ix < num_of_things; ix++) {
@@ -44,13 +45,14 @@ void* thread_func2(void* arg) {
 
     if (ix < total_things) {
         pthread_t child_thread;
-        int next_args[2]; //static array
-        next_args[0] = ix + 1; //filling the array
+        int* next_args = malloc(2 * sizeof(int));  // allocate memory
+        next_args[0] = ix + 1;                     // fill the memory
         next_args[1] = total_things;
 
         fprintf(stderr, "Thread %d (%ld) creating thread %d\n", ix, pthread_self(), ix + 1);
         pthread_create(&child_thread, NULL, thread_func2, next_args);
         pthread_join(child_thread, NULL);
+        free(next_args);
     }
 
     fprintf(stderr, "Thread %d (%ld) exiting\n", ix, pthread_self());
@@ -58,9 +60,10 @@ void* thread_func2(void* arg) {
 }
 
 void pattern_2(int num_of_things) {
-    int args[2]; //static array
-    args[0] = 1; //filling the array
+    int* args = malloc(2 * sizeof(int));  // allocate memory
+    args[0] = 1;                          // fill the memory
     args[1] = num_of_things;
     thread_func2(args);
+    free(args);
     fprintf(stderr, "\n");
 }
